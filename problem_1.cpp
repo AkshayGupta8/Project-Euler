df6cc91f85202f8c8ffe7e775a389f9e3f9007e9
AkshayGupta8 <akshay.g@icloud.com>
2022-03-12 02:36:05 -0500

    completed problem 1

--- problem_1.cpp
+++ problem_1.cpp
@@ -13,6 +13,16 @@
 
 using namespace std;
 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 int main() {
-    cout << "hello world" << endl;
+    int sum = 0;
+    for (int i = 1; i < 1000; ++i) { // iterate through all of the natural below 1000
+        if (i % 3 == 0 or i % 5 == 0) { // i is a multiple of 3 or 5
+            sum += i;
+        }
+    }
+    cout << sum << endl; // output the solution
 }
