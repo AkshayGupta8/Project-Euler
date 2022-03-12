3c2b4e968bfc3006788506aad18426c09fd13203
AkshayGupta8 <akshay.g@icloud.com>
2022-03-12 02:32:55 -0500

    added makefile and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Project-Euler
+This is my attempt at Project-Euler. I am planning to use a variety of languages to complete this project.
---
+++ problem_1.cpp
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+}
