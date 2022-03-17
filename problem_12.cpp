2d7be00351be5bc7c52f980c07245241295f6fee
AkshayGupta8 <akshay.g@icloud.com>
2022-03-17 00:11:06 -0600

    changed problem 12 again

--- problem_12.cpp
+++ problem_12.cpp
@@ -27,8 +27,6 @@ using namespace std;
 // We can see that 28 is the first triangle number to have over five divisors.
 // What is the value of the first triangle number to have over five hundred divisors?
 
-
-
 int main() {
     
 }
