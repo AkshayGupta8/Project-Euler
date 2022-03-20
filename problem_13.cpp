af4fc532a85eeb4d9f21f45b4afa2f08db665b9e
AkshayGupta8 <akshay.g@icloud.com>
2022-03-19 21:19:59 -0500

    realized I could do this problem in c++ using vectors instead of a brute force solution using python with its arbitrary precision variables

---
+++ problem_13.cpp
@@ -0,0 +1,24 @@
+#include <limits.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+// Problem Description:
+// Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+
+int main() {
+    vector<vector<int>> sum;
+    sum.resize(100, vector<int>(50));
+}
+
