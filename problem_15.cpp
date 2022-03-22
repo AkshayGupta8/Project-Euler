c4e8c465cac574bd764e37f12fc29d394b861894
AkshayGupta8 <akshay.g@icloud.com>
2022-03-22 15:43:01 -0400

    completed problem 15

---
+++ problem_15.cpp
@@ -0,0 +1,36 @@
+#include <limits.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+// Problem Description:
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+// How many such routes are there through a 20×20 grid?
+
+int main () {
+    vector<vector<long long>> matrix;
+    matrix.resize(21, vector<long long>(21));
+
+    for (int i = 0; i < matrix.size(); ++i) {
+        matrix[0][i] = 1;
+        matrix[i][0] = 1;
+    }
+
+    for (int row = 1; row < matrix.size(); ++row) {
+        for (int col = 1; col < matrix.size(); ++col) {
+            matrix[row][col] = matrix[row - 1][col] + matrix[row][col - 1];
+        }
+    }
+
+    cout << matrix.back().back() << endl;
+}
